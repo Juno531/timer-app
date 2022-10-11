e77b092457ef96ccfc6de536278d63db17a44291
Ji99 <rksk0531@naver.com>
2022-10-11 20:35:30 +0900

    commit

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from './Navigation.module.css';
 import { ReactComponent as TimerSvg } from '../assets/svg/timer.svg';
 import { ReactComponent as StopWatchSvg } from '../assets/svg/stopwatch.svg';
@@ -6,24 +7,28 @@ import { ReactComponent as StopWatchSvg } from '../assets/svg/stopwatch.svg';
 function Navigation() {
   return (
     <div className={styled.naviContainer}>
-      <div className={`${styled.menuContainer} ${styled.timerContainer}`}>
-        <div className={`${styled.timer} ${styled.naviItem}`}>
-          <TimerSvg className={`${styled.timerIcon} ${styled.naviIcon}`} />
-          <span className={`${styled.timerText} ${styled.naviText}`}>
-            타이머
-          </span>
+      <NavLink to="/" className={styled.menuContainer}>
+        <div className={`${styled.menuContainer} ${styled.timerContainer}`}>
+          <div className={`${styled.timer} ${styled.naviItem}`}>
+            <TimerSvg className={`${styled.timerIcon} ${styled.naviIcon}`} />
+            <span className={`${styled.timerText} ${styled.naviText}`}>
+              타이머
+            </span>
+          </div>
         </div>
-      </div>
-      <div className={`${styled.menuContainer} ${styled.stopWatchContainer}`}>
-        <div className={`${styled.stopWatch} ${styled.naviItem}`}>
-          <StopWatchSvg
-            className={`${styled.stopWatchIcon} ${styled.naviIcon}`}
-          />
-          <span className={`${styled.stopWatchText} ${styled.naviText}`}>
-            스탑워치
-          </span>
+      </NavLink>
+      <NavLink to="/stopwatch" className={styled.menuContainer}>
+        <div className={`${styled.menuContainer} ${styled.stopWatchContainer}`}>
+          <div className={`${styled.stopWatch} ${styled.naviItem}`}>
+            <StopWatchSvg
+              className={`${styled.stopWatchIcon} ${styled.naviIcon}`}
+            />
+            <span className={`${styled.stopWatchText} ${styled.naviText}`}>
+              스탑워치
+            </span>
+          </div>
         </div>
-      </div>
+      </NavLink>
     </div>
   );
 }
--- src/pages/Timer.tsx
+++ src/pages/Timer.tsx
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from './Timer.module.css';
 
 function Timer() {
-  return <div className={styled.TimerContainer}>timer</div>;
+  const [minutes, setMinutes] = useState(0);
+  const [tensSecond, setTensSecond] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [timerWork, setTimerWork] = useState(false);
+
+  const timerPlus = () => {
+    setMinutes(minutes + 1);
+  };
+
+  const timerMinus = () => {
+    if (minutes > 0) setMinutes(minutes - 1);
+  };
+
+  const onStartClick = () => {
+    setTimerWork(true);
+    setInterval(() => {}, 1000);
+  };
+
+  const timerTime = `${minutes}:${tensSecond}${second}`;
+  return (
+    <div className={styled.TimerContainer}>
+      <div className={styled.TimerHeader}>
+        <div className={styled.timerText}>{`${timerTime}`}</div>
+        <div className={styled.TimerHeaderButtons}>
+          <button
+            type="button"
+            className={styled.timerButton}
+            onClick={onStartClick}
+          >
+            START
+          </button>
+          <button type="button" className={styled.timerButton}>
+            STOP
+          </button>
+        </div>
+      </div>
+
+      <div className={styled.TimerMenu}>
+        <div className={styled.setTimeMenuContainer}>
+          <button
+            type="button"
+            className={`${styled.timerPlus} ${styled.setTimeMenuItem}`}
+            onClick={timerPlus}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            className={`${styled.timerMinus} ${styled.setTimeMenuItem}`}
+            onClick={timerMinus}
+          >
+            -
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Timer;
